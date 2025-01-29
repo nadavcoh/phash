4af79e4f25923675765f88ffd38daf4e36170fe3
Nadav Cohen <cohen.n@gmail.com>
2025-01-29 21:26:04 +0200

    Refactor my_sb function to improve URL handling, streamline image processing, and enhance file cleanup

--- gphoto_selenium.py
+++ gphoto_selenium.py
@@ -47,60 +47,62 @@ def my_sb(sb=None):
     
     cursor = conn.cursor()
     print("Connection Successful to PostgreSQL")
-
-    url = sb.get_current_url()
-    
-    cursor.execute("SELECT 1 FROM hashes WHERE url = %s", (url,))
-    if cursor.fetchone():
-        print("URL already exists in the database.")
-    else:
-        preview_elements = sb.find_elements("img[aria-label^='Photo']")
-        preview_element = [x for x in preview_elements if x.is_displayed()][0]
-        preview_url = preview_element.get_attribute('src')
-        label = preview_element.get_attribute('aria-label')
-
-        filename_elements = sb.find_elements("div[aria-label^='Filename']")
-        filename_element = [x for x in filename_elements if x.is_displayed()][0]
-        filename = filename_element.text
+    for i in range(1000):
+        print (i)
+        url = sb.get_current_url()
         
-        size_elements = sb.find_elements("span[aria-label^='Size:']")
-        size_element = [x for x in size_elements if x.is_displayed()][0]
-        size = size_element.text
+        cursor.execute("SELECT 1 FROM hashes WHERE url = %s", (url,))
+        if cursor.fetchone():
+            print("URL already exists in the database.")
+        else:
+            preview_elements = sb.find_elements("img[aria-label^='Photo']")
+            preview_element = [x for x in preview_elements if x.is_displayed()][0]
+            preview_url = preview_element.get_attribute('src')
+            label = preview_element.get_attribute('aria-label')
 
-        filesize_elements = sb.find_elements("span[aria-label^='File size:']")
-        filesize_element = [x for x in filesize_elements if x.is_displayed()][0]
-        filesize = filesize_element.text
-    
-        sb.send_keys("html", Keys.SHIFT + "d")
-        sleep(5)
-        hashInt = 0
+            filename_elements = sb.find_elements("div[aria-label^='Filename']")
+            filename_element = [x for x in filename_elements if x.is_displayed()][0]
+            filename = filename_element.text
+            
+            size_elements = sb.find_elements("span[aria-label^='Size:']")
+            size_element = [x for x in size_elements if x.is_displayed()][0]
+            size = size_element.text
+
+            filesize_elements = sb.find_elements("span[aria-label^='File size:']")
+            filesize_element = [x for x in filesize_elements if x.is_displayed()][0]
+            filesize = filesize_element.text
         
-        download_path = sb.get_downloads_folder()
+            sb.send_keys("html", Keys.SHIFT + "d")
+            sleep(5)
+            hashInt = 0
+            
+            download_path = sb.get_downloads_folder()
 
-        os.listdir(download_path)
-        latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
-        if latest_file.endswith('.zip'):
-            with zipfile.ZipFile(latest_file, 'r') as zip_ref:
-                zip_ref.extractall(download_path)
-            os.remove(latest_file)
+            os.listdir(download_path)
             latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
-        if latest_file.endswith('.mov'):
+            if latest_file.endswith('.zip'):
+                with zipfile.ZipFile(latest_file, 'r') as zip_ref:
+                    zip_ref.extractall(download_path)
+                os.remove(latest_file)
+                latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
+            if latest_file.endswith('.mov'):
+                os.remove(latest_file)
+                latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
+
+            img = Image.open(latest_file)
+            imgHash = str(imagehash.phash(img))
+            hashInt = twos_complement(imgHash, 64) #convert from hexadecimal to 64 bit signed integer
+
             os.remove(latest_file)
-            latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
 
-        img = Image.open(latest_file)
-        imgHash = str(imagehash.phash(img))
-        hashInt = twos_complement(imgHash, 64) #convert from hexadecimal to 64 bit signed integer
-
-        os.remove(latest_file)
-
-        cursor.execute(
-            "INSERT INTO hashes(hash, url, preview_url, label, filename, size, filesize) VALUES (%s, %s, %s, %s, %s, %s, %s)",
-            (hashInt, url, preview_url, label, filename, size, filesize)
-        )
-        conn.commit()
-        print(f"added image to database")
-        # print(url, preview_url, label, filename, size, filesize)
+            cursor.execute(
+                "INSERT INTO hashes(hash, url, preview_url, label, filename, size, filesize) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+                (hashInt, url, preview_url, label, filename, size, filesize)
+            )
+            conn.commit()
+            print(f"added image to database")
+            # print(url, preview_url, label, filename, size, filesize)
+        sb.send_keys("html", Keys.ARROW_RIGHT)
 
     return context, sb, conn, cursor
 
