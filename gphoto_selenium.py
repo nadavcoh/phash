1edb70bb65f42af02c2a2314dd77fdef4ff48994
nadavcoh <cohen.n@gmail.com>
2025-01-29 17:24:01 +0200

    postgres

--- gphoto_selenium.py
+++ gphoto_selenium.py
@@ -2,7 +2,36 @@ from seleniumbase import SB
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+import psycopg2
+import os
+from io import BytesIO
+from PIL import Image
+import imagehash
+import json
+import urllib.parse
+
+def twos_complement(hexstr, bits):
+        value = int(hexstr,16) #convert hexadecimal to integer
+
+		#convert from unsigned number to signed number with "bits" bits
+        if value & (1 << (bits-1)):
+            value -= 1 << bits
+        return value
+
 def my_sb():
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+    conn = psycopg2.connect(
+        database=config["DB_NAME"],
+        user=config["DB_USER"],
+        password=config["DB_PASSWORD"],
+        host=config["DB_HOST"]
+    )
+    
+    cursor = conn.cursor()
+    print("Connection Successful to PostgreSQL")
+
     context = SB(uc=True)
     sb = context.__enter__()
 
@@ -37,7 +66,23 @@ def my_sb():
  
     # sb.send_keys("html", Keys.SHIFT + "d")
 
-    print(url, preview_url, label, filename, size, filesize)
+    sb.download_file(preview_url)
+    parsed_url = urllib.parse.urlparse(preview_url)
+    filename = parsed_url.path.split('/')[-1]
+    sb.assert_downloaded_file(filename)
+    filepath = sb.get_path_of_downloaded_file(filename)
+    with open(filepath, "rb") as imageBinary:
+        img = Image.open(imageBinary)
+        imgHash = str(imagehash.phash(img))
+        hashInt = twos_complement(imgHash, 64) #convert from hexadecimal to 64 bit signed integer
+
+    cursor.execute(
+        "INSERT INTO hashes(hash, url, preview_url, label, filename, size, filesize) VALUES (%s, %s, %s, %s, %s, %s, %s)",
+        (hashInt, url, preview_url, label, filename, size, filesize)
+    )
+    conn.commit()
+    print(f"added image to database")
+    # print(url, preview_url, label, filename, size, filesize)
     
     return context, sb
 
