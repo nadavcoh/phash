35a3783e262b351cd082c91c71ffe0c3309bcf59
nadavcoh <cohen.n@gmail.com>
2025-01-29 19:21:25 +0200

    Enhance file handling in my_sb function to support zip extraction and cleanup of temporary files

--- gphoto_selenium.py
+++ gphoto_selenium.py
@@ -6,9 +6,13 @@ import psycopg2
 import os
 from io import BytesIO
 from PIL import Image
+from pillow_heif import register_heif_opener
 import imagehash
 import json
+import zipfile
 import urllib.parse
+
+register_heif_opener()
 
 def twos_complement(hexstr, bits):
         value = int(hexstr,16) #convert hexadecimal to integer
@@ -51,17 +55,27 @@ def my_sb():
     filesize_element = [x for x in filesize_elements if x.is_displayed()][0]
     filesize = filesize_element.text
  
-    # sb.send_keys("html", Keys.SHIFT + "d")
+    sb.send_keys("html", Keys.SHIFT + "d")
+    hashInt = 0
+    
+    download_path = sb.get_downloads_folder()
 
-    sb.download_file(preview_url)
-    parsed_url = urllib.parse.urlparse(preview_url)
-    filename = parsed_url.path.split('/')[-1]
-    sb.assert_downloaded_file(filename)
-    filepath = sb.get_path_of_downloaded_file(filename)
-    with open(filepath, "rb") as imageBinary:
-        img = Image.open(imageBinary)
-        imgHash = str(imagehash.phash(img))
-        hashInt = twos_complement(imgHash, 64) #convert from hexadecimal to 64 bit signed integer
+    os.listdir(download_path)
+    latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
+    if latest_file.endswith('.zip'):
+        with zipfile.ZipFile(latest_file, 'r') as zip_ref:
+            zip_ref.extractall(download_path)
+        os.remove(latest_file)
+        latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
+    if latest_file.endswith('.mov'):
+        os.remove(latest_file)
+        latest_file = max([os.path.join(download_path, f) for f in os.listdir(download_path)], key=os.path.getctime)
+
+    img = Image.open(latest_file)
+    imgHash = str(imagehash.phash(img))
+    hashInt = twos_complement(imgHash, 64) #convert from hexadecimal to 64 bit signed integer
+
+    os.remove(latest_file)
 
     with open('config.json') as config_file:
         config = json.load(config_file)
@@ -83,9 +97,9 @@ def my_sb():
     conn.commit()
     print(f"added image to database")
     # print(url, preview_url, label, filename, size, filesize)
-    sb.delete_downloaded_file_if_present(filename)
-    return context, sb
+
+    return context, sb, conn, cursor
 
 if __name__ == '__main__':
-    context, sb = my_sb()
+    context, sb, conn, cursor = my_sb()
     context.__exit__(None, None, None)
